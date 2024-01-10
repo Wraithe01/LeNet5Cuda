f80b2cc72cbc042db5911b5291ca9ab873898e2e
wraithe <wraithe01@hotmail.com>
2024-01-10 22:12:42 +0100

    pref(): microoptimisations

--- LeNet-5Cuda/lenet.cu
+++ LeNet-5Cuda/lenet.cu
@@ -369,13 +369,13 @@ void ConvolutionBackward(double* input, double* inError, double* outError, doubl
 								(input, weightDeltas, outError);
 }
 
-__global__ void CUDA_SubsampForward(const double const* input, double* output, const uint32_t len, const uint32_t lenFeatIn, const uint32_t lenFeatOut)
+__global__ void CUDA_SubsampForward(const double const* input, double* output, const uint32_t len, const uint32_t lenFeatIn, const uint32_t lenFeatOut, const uint32_t maxZ)
 {
 	const uint32_t o1 = threadIdx.x + blockIdx.x * blockDim.x;
 	const uint32_t o0 = threadIdx.y + blockIdx.y * blockDim.y;
 	const uint32_t i = threadIdx.z + blockIdx.z * blockDim.z;
 
-	if (o0 < lenFeatOut && o1 < lenFeatOut)
+	if (o0 < lenFeatOut && o1 < lenFeatOut && i < maxZ)
 	{
 		int32_t x0 = 0;
 		int32_t x1 = 0;
@@ -401,23 +401,23 @@ void SubsampForward(double* input, double* output, size_t insize, size_t inlayer
 
 	uint32_t len = inlayersize / outlayersise;
 	{
-		dim3 threads = { 16, 16, 1 };
+		dim3 threads = { 16, 16, 4 };
 		dim3 blocks = {
 			(uint32_t)ceil((double)outlayersise / threads.x),
 			(uint32_t)ceil((double)outlayersise / threads.y),
 			(uint32_t)ceil((double)outsize / threads.z)
 		};
-		CUDA_SubsampForward << <blocks, threads >> > (input, output, len, inlayersize, outlayersise);
+		CUDA_SubsampForward << <blocks, threads >> > (input, output, len, inlayersize, outlayersise, outsize);
 	}
 }
 
-__global__ void CUDA_SubsampBackward(const double const* input, double* inerror, double* outerror, const uint32_t len, const uint32_t lenFeatIn, const uint32_t lenFeatOut)
+__global__ void CUDA_SubsampBackward(const double const* input, double* inerror, const double const* outerror, const uint32_t len, const uint32_t lenFeatIn, const uint32_t lenFeatOut, const uint32_t maxZ)
 {
 	const uint32_t o1 = threadIdx.x + blockIdx.x * blockDim.x;
 	const uint32_t o0 = threadIdx.y + blockIdx.y * blockDim.y;
 	const uint32_t i = threadIdx.z + blockIdx.z * blockDim.z;
 
-	if (o0 < lenFeatOut && o1 < lenFeatOut)
+	if (o0 < lenFeatOut && o1 < lenFeatOut && i < maxZ)
 	{
 		int32_t x0 = 0;
 		int32_t x1 = 0;
@@ -441,13 +441,13 @@ void SubsampBackward(double* input, double* inerror, double* outerror, size_t in
 {
 	uint32_t len = inlayersize / outlayersise;
 	{
-		dim3 threads = { 16, 16, 1 };
+		dim3 threads = { 16, 16, 4 };
 		dim3 blocks = {
 			(uint32_t)ceil((double)outlayersise / threads.x),
 			(uint32_t)ceil((double)outlayersise / threads.y),
 			(uint32_t)ceil((double)outsize / threads.z)
 		};
-		CUDA_SubsampBackward << <threads, blocks >> > (input, inerror, outerror, len, inlayersize, outlayersise);
+		CUDA_SubsampBackward << <threads, blocks >> > (input, inerror, outerror, len, inlayersize, outlayersise, outsize);
 	}
 }
 
